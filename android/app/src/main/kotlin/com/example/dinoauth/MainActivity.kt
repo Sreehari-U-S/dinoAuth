693dabb117c5b3312d8c454c5a57ec8eed76f2b2
SandhraPrathap <sandhraprathap@gmail.com>
2020-09-22 22:38:05 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/dinoauth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dinoauth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Dino extends StatefulWidget {
+  @override
+  _DinoState createState() => _DinoState();
+}
+
+class _DinoState extends State<Dino> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Dino(),
+  ));
+}
